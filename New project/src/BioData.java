2b195105078b4ab95bf9bfef04409a296dea691a
bsrinvias <thisisseenu@gmail.com>
2017-07-06 12:45:26 +0530

    C
    C

---
+++ New project/src/BioData.java
@@ -0,0 +1,36 @@
+
+public class BioData {
+
+public static void Name(){
+System.out.println("Sreenivasulu");
+}
+
+public static void Class(){
+	System.out.println("MSC(is)");
+}
+
+public static void Course(){
+	System.out.println("Computers");
+}
+
+public void College(){
+	System.out.println("St jpesph");
+}
+
+public  void Office(){
+	System.out.println("EPAM");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		BioData.Name();
+		BioData.Class();
+		BioData.Course();
+		BioData Col=new BioData();
+		Col.College();
+		Col.Office();
+	}
+	
+}
+
+
---
+++ New project/src/IFStatement.java
@@ -0,0 +1,24 @@
+
+public class IFStatement {
+
+	public static void main(String[] args) {
+
+		int num=25;
+		Verifynum(num);
+
+	}
+
+	private static void Verifynum(int num) {
+		if(num%2==0){
+			System.out.println("Even");
+			
+					} 
+		else  {
+		System.out.println("odd");
+		}	
+					
+			
+		
+	}
+
+}
